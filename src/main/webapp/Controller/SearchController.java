0af72d4bc6351d38b17bfaca5c0c72e6d2c0f8e1
Eric Duguay <eduguay@usc.edu>
2019-02-28 21:41:58 -0800

    Removed key

--- src/main/webapp/Controller/SearchController.java
+++ src/main/webapp/Controller/SearchController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /*
- *  Google Custom Search API Key: AIzaSyBKL46dI7C9lgxkPQXK63DNdZeK_8Tw06k
+ *  Google Custom Search API Key: 
  */
 
 
