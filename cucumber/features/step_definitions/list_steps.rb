f14ce174bf2d10e012296b34ce2a1081e426aee1
xuyangzo <xuyangzo@usc.edu>
2019-03-08 03:23:04 -0800

    modification on cucumber

--- cucumber/features/step_definitions/list_steps.rb
+++ cucumber/features/step_definitions/list_steps.rb
@@ -26,13 +26,51 @@ Given(/^User will enter "coffee" and "7" and click Feed Me! and adds several ite
     find('.return-to').click
     wait_for_ajax
     # add restaurant 1 to Favorite List and To Explore List
-    restaurant_name1 = find('.restaurant-0 .result-restaurant-item-name').text
-    find('.restaurant-0').click
+    restaurant_name1 = find('.restaurant-1 .result-restaurant-item-name').text
+    find('.restaurant-1').click
     find('.select-list').click
     find('.item-1').click
     find('.add-list').click
     find('.select-list').click
     find('.item-2').click
+    find('.add-list').click
+    find('.return-to').click
+    wait_for_ajax
+    # add restaurant 2 to Do Not Show List
+    restaurant_name2 = find('.restaurant-2 .result-restaurant-item-name').text
+    find('.restaurant-2').click
+    find('.select-list').click
+    find('.item-3').click
+    find('.add-list').click
+    find('.return-to').click
+    wait_for_ajax
+    # add recipe 0 to Favorite List and To Explore List
+    recipe_name0 = find('.recipe-0 .result-recipe-item-name').text
+    find('.recipe-0').click
+    find('.select-list').click
+    find('.item-1').click
+    find('.add-list').click
+    find('.select-list').click
+    find('.item-2').click
+    find('.add-list').click
+    find('.return-to').click
+    wait_for_ajax
+    # add recipe 1 to Favorite List and To Explore List
+    recipe_name1 = find('.recipe-1 .result-recipe-item-name').text
+    find('.recipe-1').click
+    find('.select-list').click
+    find('.item-1').click
+    find('.add-list').click
+    find('.select-list').click
+    find('.item-2').click
+    find('.add-list').click
+    find('.return-to').click
+    wait_for_ajax
+    # add recipe 2 to Do Not Show List
+    recipe_name2 = find('.recipe-2 .result-recipe-item-name').text
+    find('.recipe-2').click
+    find('.select-list').click
+    find('.item-3').click
     find('.add-list').click
     find('.return-to').click
     wait_for_ajax
